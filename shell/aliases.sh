af98984bb1099d911a5e86f1eb61c78566533e27
Berdimyradov <kberdimyradov@gmail.com>
2024-10-31 09:49:45 +0300

    :technologist: Add lfcat alias to zshrc

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,9 +1,11 @@
 # Example aliases
-alias ls="colorls"
+# alias ls="colorls"
 alias vim="nvim"
 alias zshconfig="vim ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 # alias prettyjson="python -m json.tool"
+
+alias lfcat='find . -type f -exec echo "Filename: {}" \; -exec cat {} \;'
 
 # bindkey -v
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
