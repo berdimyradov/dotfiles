bfae6aebf10378ea18246de93682af01b59e5910
Berdimyradov <kberdimyradov@gmail.com>
2026-01-17 14:45:33 +0300

    :hammer: Add  script for chaning alacritty's theme

---
+++ shell/alacritty/init.sh
@@ -0,0 +1,1 @@
+alias theme="$HOME/shell/alacritty/theme.sh"
--- shell/init.sh
+++ shell/init.sh
@@ -1,10 +1,18 @@
+# Main
+
+# Zsh
 source ~/shell/zsh/init.sh
 
+# General
 source ~/shell/aliases.sh
 source ~/shell/exports.sh
 source ~/shell/fzf.sh
 source ~/shell/git/init.sh
 
+# Terminal
+source ~/shell/alacritty/init.sh
+
+# Optional
 # source ~/shell/docker.sh
 # source ~/.rcs/flutter.sh
 # source ~/.rcs/randy.sh
