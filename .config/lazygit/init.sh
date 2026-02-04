cb0bbc7ab659908414f23d5be8f023fb1322f6c8
Berdimyradov <kberdimyradov@gmail.com>
2026-02-04 15:47:37 +0300

    :heavy_plus_sign: Add catpuccin themes for lazygit

--- .config/lazygit/init.sh
+++ .config/lazygit/init.sh
@@ -1,9 +1,25 @@
-alias lazygit:light='lazygit --use-config-file="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/light.yml"'
-alias lazygit:dark='lazygit --use-config-file="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/dark.yml"'
+# Variables
+LG_CONF="$HOME/.config/lazygit/config.yml"
+LG_THEMES="$HOME/.config/lazygit/themes"
 
-# ────────────────────────────────────────────────────────────────────────────────
+# Default
+# alias lazygit:light='lazygit --use-config-file="$LG_CONF,$LG_THEMES/light.yml"'
+# alias lazygit:dark='lazygit --use-config-file="$LG_CONF,$LG_THEMES/dracula.dark.yml"'
+
+# Catppuccin
+# alias lazygit:light='lazygit --use-config-file="$LG_CONF,$LG_THEMES/catppuccin/latte/blue.yml"'
+# alias lazygit:dark='lazygit --use-config-file="$LG_CONF,$LG_THEMES/catppuccin/mocha/blue.yml"'
+
+# ================================================================================
+# CURRENT
+# ================================================================================
+alias lazygit:light='lazygit --use-config-file="$LG_CONF,$LG_THEMES/light.yml"'
+alias lazygit:dark='lazygit --use-config-file="$LG_CONF,$LG_THEMES/dracula.dark.yml"'
+
+# ================================================================================
 # Lazy Utilities
-# ────────────────────────────────────────────────────────────────────────────────
+# ================================================================================
 alias lnpm='lazynpm'
 alias ldocker='lazydocker'
 alias lsql='lazysql'
+
