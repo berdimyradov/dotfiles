28b5daccc036ea31d4dc3fbf49294d9818ba7ec5
Berdimyradov <kberdimyradov@gmail.com>
2026-02-04 14:09:58 +0300

    :lipstick: Enable theme switching for lazygit

--- .config/lazygit/TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO
-
-- [ ] create light.config.yml
-- [ ] create dark.config.yml
-- [ ] create symlink config.yml which is depended on light/dark theme
---
+++ .config/lazygit/init.sh
@@ -0,0 +1,9 @@
+alias lazygit:light='lazygit --use-config-file="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/light.yml"'
+alias lazygit:dark='lazygit --use-config-file="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/dark.yml"'
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Lazy Utilities
+# ────────────────────────────────────────────────────────────────────────────────
+alias lnpm='lazynpm'
+alias ldocker='lazydocker'
+alias lsql='lazysql'
