4c6c0f59420fba17ad73cabe4a6737e5156d64ad
Berdimyradov <kberdimyradov@gmail.com>
2026-06-28 18:20:20 +0300

    :recycle: refactor diffview config with toggle logic and file history

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,31 +1,42 @@
 return {
-  "sindrets/diffview.nvim",
-  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-  config = true,
-
-  -- dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    enhanced_diff_hl = true, -- Better diff highlighting
-
-    -- Enables filetype icons in the Diffview panels (requires `nvim-web-devicons` or `mini.icons`)
-    use_icons = true,
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>gd", false, mode = "n" },
+    },
   },
 
-  keys = {
-    -- { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
-    -- Simple toggle logic: closes if open, opens if closed
-    {
-      "<leader>gd",
-      function()
-        if next(require("diffview.lib").views) == nil then
-          vim.cmd("DiffviewOpen")
-        else
-          vim.cmd("DiffviewClose")
-        end
-      end,
-      desc = "Toggle Diffview",
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+
+    -- dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      enhanced_diff_hl = true, -- Better diff highlighting
+
+      -- Enables filetype icons in the Diffview panels (requires `nvim-web-devicons` or `mini.icons`)
+      use_icons = true,
     },
-    { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
-    { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
+
+    keys = {
+      -- Simple toggle logic: closes if open, opens if closed
+      {
+        "<leader>gd",
+        function()
+          local lib = require("diffview.lib")
+          local view = lib.get_current_view()
+
+          if view then
+            vim.cmd("DiffviewClose")
+          else
+            vim.cmd("DiffviewOpen")
+          end
+        end,
+        desc = "Toggle Diffview",
+      },
+
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
+    },
   },
 }
