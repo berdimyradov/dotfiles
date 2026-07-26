03b1108c0d4b9c3471a2147100d19bc45e5bef2d
Berdimyradov <kberdimyradov@gmail.com>
2026-07-26 14:41:04 +0300

    :recycle: simplify neo-tree config and add window positioning

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,42 +1,31 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts.filesystem = opts.filesystem or {}
-      opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+    opts = {
+      window = {
+        width = 40,
 
-      --
-      -- Never Show
-      --
-      local never_show = opts.filesystem.filtered_items.never_show or {}
-      local never = {
-        [".git"] = true,
-        [".DS_Store"] = true,
-      }
+        mappings = {
+          ["z"] = "none",
+        },
 
-      for _, name in ipairs(never_show) do
-        never[name] = true
-      end
-
-      opts.filesystem.filtered_items.never_show = {}
-      for name, _ in pairs(never) do
-        table.insert(opts.filesystem.filtered_items.never_show, name)
-      end
-      table.sort(opts.filesystem.filtered_items.never_show)
-
-      --
-      -- Always Show
-      --
-      local always_show = opts.filesystem.filtered_items.always_show or {}
-      local always = { [".gitignore"] = true }
-      for _, name in ipairs(always_show) do
-        always[name] = true
-      end
-      opts.filesystem.filtered_items.always_show = {}
-      for name, _ in pairs(always) do
-        table.insert(opts.filesystem.filtered_items.always_show, name)
-      end
-      table.sort(opts.filesystem.filtered_items.always_show)
-    end,
+        position = "float",
+        popup = {
+          position = { row = "50%", col = "0%" },
+          size = { height = "93%", width = "30%" },
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          never_show = {
+            ".DS_Store",
+            ".git",
+          },
+          always_show = {
+            ".gitignore",
+          },
+        },
+      },
+    },
   },
 }
